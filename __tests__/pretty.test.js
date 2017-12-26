baca386ed9f512db1d84617ec71ae978ad954c67
Patrick Hensley <pathensley@gmail.com>
2017-12-26 10:24:09 -0500

    Move test file into resources.

--- __tests__/pretty.test.js
+++ __tests__/pretty.test.js
@@ -1,15 +1,16 @@
+import { join } from 'path';
 import prettyJson from '../src/pretty';
 import { TestLoader, parseTemplate } from './loader';
 
 
-const loader = new TestLoader(__dirname, {
+const loader = new TestLoader(join(__dirname, 'resources'), {
   TEMPLATE: parseTemplate,
   PRETTY: s => s.trim()
 });
 
 
 test('basic', () => {
-  const spec = loader.load('./data/pretty.1.txt');
+  const spec = loader.load('pretty-1.txt');
   const actual = prettyJson(spec.TEMPLATE);
   expect(actual).toEqual(spec.PRETTY);
 
