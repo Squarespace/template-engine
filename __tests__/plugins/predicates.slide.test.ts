ef721827104797ca94f3a228f9407149404eadca
Patrick Hensley <pathensley@gmail.com>
2019-06-20 10:21:14 -0400

    Implement slide current-type? predicate

---
+++ __tests__/plugins/predicates.slide.test.ts
@@ -0,0 +1,9 @@
+import { join } from 'path';
+import { TemplateTestLoader } from '../loader';
+
+const loader = new TemplateTestLoader(join(__dirname, 'resources'));
+
+
+test('current-type?', () => {
+  loader.execute('p-current-type.html');
+});
--- src/plugins/predicates.slide.ts
+++ src/plugins/predicates.slide.ts
@@ -1,7 +1,18 @@
-import { PredicateTable } from '../plugin';
+import { Context } from '../context';
+import { Predicate, PredicateTable } from '../plugin';
+import { SliceType } from './enums';
 
-// TODO: current-type?  should be generalized or deprecated and replaced.
+// TODO: current-type?  should be generalized.
+
+export class CurrentTypePredicate extends Predicate {
+
+  apply(args: string[], ctx: Context): boolean {
+    const expected = ctx.node().get('currentType').asNumber() | 0;
+    const type = SliceType.fromName(args[0]);
+    return (type && type.code === expected) || false;
+  }
+}
 
 export const TABLE: PredicateTable = {
-
+  'current-type?': new CurrentTypePredicate()
 };
