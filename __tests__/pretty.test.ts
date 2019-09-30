9a828cbd86687086e986f231d72d1fec95e5f0f7
Patrick Hensley <pathensley@gmail.com>
2019-09-30 15:10:31 -0400

    Add ast tests from upstream template-compiler project

--- __tests__/pretty.test.ts
+++ __tests__/pretty.test.ts
@@ -1,19 +1,26 @@
 import { join } from 'path';
+import { pathseq } from './helpers';
 import { prettyJson } from '../src/pretty';
-import { TestLoader, parseTemplate } from './loader';
-
+import { parseTemplate, TestLoader } from './loader';
 
 const loader = new TestLoader(join(__dirname, 'resources'), {
-  TEMPLATE: parseTemplate,
-  PRETTY: (s: string) => s.trim()
+  TEMPLATE: (s: string) => parseTemplate(s.trim()),
+  JSON: (s: string) => s.trim()
 });
-
 
 test('basic', () => {
   const spec = loader.load('pretty-1.txt');
   const actual = prettyJson(spec.TEMPLATE);
-  expect(actual).toEqual(spec.PRETTY);
+  expect(actual).toEqual(spec.JSON);
 
   // Ensure re-parse of pretty-fied JSON equals the original.
   expect(JSON.parse(actual)).toEqual(spec.TEMPLATE);
+});
+
+pathseq('ast-%N.html', 17).forEach(path => {
+  test(path, () => {
+    const spec = loader.load(path);
+    const actual = prettyJson(spec.TEMPLATE);
+    expect(JSON.parse(actual)).toEqual(JSON.parse(spec.JSON));
+  });
 });
