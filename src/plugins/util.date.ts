60091f49bc322f915a81ddd44d462f6f4fc58d72
Patrick Hensley <pathensley@gmail.com>
2019-10-29 09:18:51 -0400

    Remove old code

--- src/plugins/util.date.ts
+++ src/plugins/util.date.ts
@@ -6,148 +6,7 @@ interface Calc {
 }
 
 /**
- * Maps Unix date pattern to CLDR form:
- * https://www.unicode.org/reports/tr35/tr35-dates.html#Date_Format_Patterns
- */
-// const UNIX_TO_CLDR_FORMATS: { [field: string]: string | Calc } = {
-
-//   // %a     locale's abbreviated weekday name (e.g., Sun)
-//   a: 'E',
-
-//   // %A     locale's full weekday name (e.g., Sunday)
-//   A: 'EEEE',
-
-//   // %b     locale's abbreviated month name (e.g., Jan)
-//   b: 'MMM',
-
-//   // %B     locale's full month name (e.g., January)
-//   B: 'MMMM',
-
-//   // %c     locale's date and time (e.g., Thu Mar  3 23:05:25 2005)
-//   c: 'E, MMM d, y h:mm:ss a z',
-
-//   // %C     century; like %Y, except omit last two digits (e.g., 20)
-//   C: { calc: 'century' },
-
-//   // %D     date; same as %m/%d/%y
-//   D: 'MM/dd/yy',
-
-//   // %d     day of month (e.g., 01)
-//   d: 'dd',
-
-//   // %e     day of month, space padded; same as %_d
-//   e: { calc: 'day-space' },
-
-//   // %F     full date; same as %Y-%m-%d
-//   F: 'y-MM-dd',
-
-//   // %g     last two digits of year of ISO week number (see %G)
-//   g: 'G',
-
-//   // %G     year of ISO week number (see %V); normally useful only with %V
-//   G: 'GGGG',
-
-//   // %h     same as %b
-//   h: 'MMM',
-
-//   // %H     hour (00..23)
-//   H: 'HH',
-
-//   // %I     hour (01..12)
-//   I: 'hh',
-
-//   // %j     day of year (001..366)
-//   j: 'DDD',
-
-//   // %k     hour, space padded ( 0..23); same as %H
-//   k: { calc: 'hour-24-space' },
-
-//   // %l     hour, space padded ( 1..12); same as %I
-//   l: { calc: 'hour-12-space' },
-
-//   // %m     month (01..12)
-//   m: 'MM',
-
-//   // %M     minute (00..59)
-//   M: 'mm',
-
-//   // %n     a newline
-//   n: '\n',
-
-//   // %N     nanoseconds (000000000..999999999)
-//   N: 'SSSSSSSSS',
-
-//   // %p     locale's equivalent of either AM or PM; blank if not known
-//   p: 'a',
-
-//   // %P     like %p, but lower case
-//   P: 'a',
-
-//   // %q     quarter of year (1..4)
-//   q: 'Q',
-
-//   // %r     locale's 12-hour clock time (e.g., 11:11:04 PM)
-//   r: 'h:mm:ss a',
-
-//   // %R     24-hour hour and minute; same as %H:%M
-//   R: '%H:%M',
-
-//   // %s     seconds since 1970-01-01 00:00:00 UTC
-//   s: { calc: 'epoch-seconds' },
-
-//   // %S     second (00..60)
-//   S: 'ss',
-
-//   // %t     a tab
-//   t: '\t',
-
-//   // %T     time; same as %H:%M:%S
-//   T: 'HH:mm:ss',
-
-//   // %u     day of week (1..7); 1 is Monday
-//   u: { calc: 'day-of-week-1' },
-
-//   // %U     week number of year, with Sunday as first day of week (00..53)
-//   U: '', // TODO: not supported directly in moment. %U range is 00-53, moment has no comparable field.
-
-//   // %w     day of week (0..6); 0 is Sunday
-//   w: { calc: 'day-of-week-2' },
-
-//   // %W     week number of year, with Monday as first day of week (00..53)
-//   W: 'ww',
-
-//   // %x     locale's date representation (e.g., 12/31/99)
-//   x: 'MM/dd/yy',
-
-//   // %X     locale's time representation (e.g., 23:13:48)
-//   X: 'h:mm:ss a',
-
-//   // %y     last two digits of year (00..99)
-//   y: 'yy',
-
-//   // %Y     year
-//   Y: 'y',
-
-//   // %z     +hhmm numeric time zone (e.g., -0400)
-//   z: 'ZZ',
-
-//   // %Z     alphabetic time zone abbreviation (e.g., EDT)
-//   Z: 'z',
-
-//   // Undocumented
-//   v: 'd',
-
-//   // %V     ISO week number, with Monday as first day of week (01..53)
-//   V: { calc: 'iso-week-number' },
-
-//   // NOT IMPLEMENTED:
-//   // %:z    +hh:mm numeric time zone (e.g., -04:00)
-//   // %::z   +hh:mm:ss numeric time zone (e.g., -04:00:00)
-//   // %:::z  numeric time zone with : to necessary precision (e.g., -04, +05:30)
-// };
-
-/**
- * Translate a YUI / UNIX date format to CLDR format.
+ * Formats a date using a YUI / UNIX pattern.
  */
 export const formatDate = (d: GregorianDate, fmt: string) => {
   let esc = '';
@@ -427,11 +286,6 @@ export const formatDate = (d: GregorianDate, fmt: string) => {
       parts.push(out);
     }
 
-    // const replacement = UNIX_TO_CLDR_FORMATS[ch];
-    // if (replacement) {
-    //   parts.push(replacement);
-    // }
-
     i++;
   }
 
@@ -453,8 +307,6 @@ const getTZC = (offset: number): [boolean, number, number] => {
   const minutes = offset % 60;
   return [negative, hours, minutes];
 };
-
-// const pad = (str: string, c: string = ' ') => str.length === 2 ? str : `${c}${str}`;
 
 const pad = (s: string, ch: string, n: number) => {
   let d = n - s.length;
@@ -486,94 +338,6 @@ const LONG_MONTHS = [
   'November',
   'December'
 ];
-
-/**
- * We translate the old YUI Unix datetime pattern into CLDR patterns that
- * can be formatted using the raw formatter.
- *
- * Argument 'd' is a GregorianDate instance.
- */
-
-/*
- export const formatDate = (d: GregorianDate, unixfmt: string) => {
-  const res: string[] = [];
-  const parts = translateUnixToCLDR(unixfmt);
-  const len = parts.length;
-  for (let i = 0; i < len; i++) {
-    let out: string = '';
-    const part = parts[i];
-    if (typeof part === 'string') {
-      switch (part) {
-        case 'E':
-          out = SHORT_DAYS[d.dayOfWeek() - 1];
-          break;
-        case 'EEEE':
-          out = LONG_DAYS[d.dayOfWeek() - 1];
-          break;
-        case 'MMM':
-          out = SHORT_MONTHS[d.month() - 1];
-          break;
-        case 'MMMM':
-          out = LONG_MONTHS[d.month() - 1];
-          break;
-        case 'E, MMM d, y h:mm:ss a z':
-          out = formatDate(d, part);
-          break;
-        case 'MM/dd/yy':
-
-        default:
-          out = part;
-          if (part[0] === "'") {
-            out = part.substring(1, part.length - 1);
-          }
-          break;
-      }
-
-    } else {
-      switch (part.calc) {
-        case 'century':
-          out = `${(d.year() / 100) | 0}`;
-          break;
-
-        case 'day-of-week-1': {
-          const wk = d.dayOfWeek() - 1;
-          out = wk === 0 ? '7' : `${wk}`;
-          break;
-        }
-
-        case 'day-of-week-2': {
-          out = `${d.dayOfWeek() - 1}`;
-          break;
-        }
-
-        case 'day-space':
-          out = pad(`${d.dayOfMonth()}`);
-          break;
-
-        case 'epoch-seconds':
-          out = `${(d.unixEpoch() / 1000) | 0}`;
-          break;
-
-        case 'hour-12-space':
-          out = pad(`${d.hour()}`);
-          break;
-
-        case 'hour-24-space':
-          out = pad(`${d.hourOfDay()}`);
-          break;
-
-        case 'iso-week-number':
-          // TODO: requires date conversion to iso-8601 calendar currently
-          out = '';
-          break;
-      }
-      res.push(out);
-    }
-  }
-  return parts.join('');
-};
-
-*/
 
 /**
  * Retrieves the Website's timeZone from the context, falling
