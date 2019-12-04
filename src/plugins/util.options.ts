8eca408e766ac1185f4ff682b7ed99ca5b383b7e
Patrick Hensley <pathensley@gmail.com>
2019-12-04 10:22:44 -0500

    Datetime formatter options compatibility

--- src/plugins/util.options.ts
+++ src/plugins/util.options.ts
@@ -14,95 +14,106 @@ const calendarFormat = (key: string) => {
 
 export const setCalendarFormatOptions = (args: string[]) => {
   const opts: any = {};
-  // let format = undefined;
+  let skeleton = '';
   for (const arg of args) {
     const i = arg.indexOf(':');
     if (i === -1) {
       switch (arg) {
-      case 'date':
-      case 'time':
-        opts[arg] = 'short';
-        break;
+        case 'date':
+        case 'time':
+          opts[arg] = 'short';
+          break;
 
-      default:
-        // format = calendarFormat(arg);
-        // if (format !== undefined) {
-        //   opts.date = format;
-        //   opts.time = format;
-        // } else {
-
-        // }
-        break;
-        // TODO: Skeletons
+        default: {
+          const format = calendarFormat(arg);
+          if (format) {
+            opts.date = format;
+            opts.time = format;
+          } else {
+            skeleton += format;
+          }
+          break;
+        }
       }
+
       continue;
     }
 
     const key = arg.substring(0, i);
     const val = arg.substring(i + 1);
     switch (key) {
-    case 'date':
-    case 'time':
-    case 'datetime':
-      opts[key] = val;
-      break;
+      case 'date':
+      case 'time': {
+        const format = calendarFormat(val);
+        if (format) {
+          opts[key] = val;
+        } else {
+          skeleton += format;
+        }
+        break;
+      }
 
-    case 'wrap':
-    case 'wrapper':
-    case 'wrapped':
-      opts.wrap = val;
-      break;
+      case 'wrap':
+      case 'wrapper':
+      case 'wrapped':
+        opts.wrap = val;
+        break;
     }
   }
-  return opts;
+
+  if (skeleton) {
+    opts.skeleton = skeleton;
+  }
+
+  return Object.keys(opts).length ? opts : { skeleton: 'yMd' };
 };
 
 /*eslint complexity: ["error", 24]*/
 export const setNumberFormatOption = (opts: any, arg: string, val: any) => {
   switch (arg) {
-  case 'group':
-  case 'grouping':
-    opts.group = true;
-    break;
-
-  case 'round':
-  case 'rounding':
-    switch (val) {
-    case 'ceil':
-    case 'truncate':
-    case 'floor':
-    case 'round':
-    case 'half-even':
-    case 'default':
-      opts.round = val;
+    case 'group':
+    case 'grouping':
+      opts.group = true;
       break;
-    }
-    break;
 
-  case 'minint':
-  case 'minInt':
-    opts.minimumIntegerDigits = toInt(val);
-    break;
+    case 'round':
+    case 'rounding':
+      switch (val) {
+        case 'ceil':
+        case 'truncate':
+        case 'floor':
+        case 'round':
+        case 'half-even':
+        case 'default':
+          opts.round = val;
+          break;
+      }
+      break;
 
-  case 'maxfrac':
-  case 'maxFrac':
-    opts.maximumFractionDigits = toInt(val);
-    break;
+    case 'minint':
+    case 'minInt':
+      opts.minimumIntegerDigits = toInt(val);
+      break;
 
-  case 'minfrac':
-  case 'minFrac':
-    opts.minimumFractionDigits = toInt(val);
-    break;
+    case 'maxfrac':
+    case 'maxFrac':
+      opts.maximumFractionDigits = toInt(val);
+      break;
 
-  case 'minsig':
-  case 'minSig':
-    opts.minimumSignificantDigits = toInt(val);
-    break;
+    case 'minfrac':
+    case 'minFrac':
+      opts.minimumFractionDigits = toInt(val);
+      break;
 
-  case 'maxsig':
-  case 'maxSix':
-    opts.maximumSignificantDigits = toInt(val);
-    break;
+    case 'minsig':
+    case 'minSig':
+      opts.minimumSignificantDigits = toInt(val);
+      break;
+
+    case 'maxsig':
+    case 'maxSix':
+      opts.maximumSignificantDigits = toInt(val);
+      break;
   }
 };
 
